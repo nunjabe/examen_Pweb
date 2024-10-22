2fa8a2c505ff8a30931571b6df102930fd2ee2b8
CDER <134414589+nunjabe@users.noreply.github.com>
2024-10-21 18:43:51 -0600

    Add files via upload

---
+++ Objeto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea.Models
+{
+    public class Objeto
+    {
+        // Propiedad para identificar el producto de forma única
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El número de inventario es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El número de inventario debe ser un número entero positivo.")]
+        public int? NumeroInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [MaxLength(200, ErrorMessage = "El nombre no puede exceder 200 caracteres.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio.")]
+        [RegularExpression("Frutas|Verduras|Lácteos",
+            ErrorMessage = "El departamento debe ser 'Frutas', 'Verduras' o 'Lácteos'.")]
+        public string? Departamento { get; set; }
+    }
+}
